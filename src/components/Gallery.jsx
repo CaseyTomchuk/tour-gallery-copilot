1f9300a8b34fdd769a1e8601f5685cc6a939c228
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-04-27 15:18:00 -0400

    Troubleshooting - Passing handleRemove

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ const Gallery = ({ tours, handleRemove }) => {
     return (
         <div className="gallery">
             {tours.map((tour) => (
-                <TourCard key={tour.id} tour={tour} />
+                <TourCard key={tour.id} tour={tour} handleRemove ={handleRemove} />
             ))}
         </div>
     );
@@ -15,3 +15,5 @@ export default Gallery;
 
 // Copilot was able to remember the instructions to focus on functionality first!
 // Had to manually add handleRemove to Gallery so it coudl be passed to TourCard.
+
+// Troubleshooting: had to pass handleRemove to TourCard. Copilot struggles with connecting components.
